cf25e7a4cbdcc8af4ab4a10f2ffb3c7268e2293a
sunny parmar <shree214ram@gmail.com>
2021-05-21 15:04:24 +0530

    Create sapna.js

---
+++ sapna.js
@@ -0,0 +1,3 @@
+let current_datetime = new Date()
+let DATEFORMATTER_YYYY_MM_DD = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() ;
+let FORMATTER_YYYY_MM_DD_HH_mm_ss_n = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds();
