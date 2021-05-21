bc53cbf7f66ad7b3a308edafd1a71de55d30a86a
sunny parmar <shree214ram@gmail.com>
2021-05-21 15:18:50 +0530

    Update sapna.js

--- sapna.js
+++ sapna.js
@@ -1,8 +1,9 @@
 export default class sapna {
   function sapna(){
-    let current_datetime = new Date()
-    let DATEFORMATTER_YYYY_MM_DD = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() ;
-    let FORMATTER_YYYY_MM_DD_HH_mm_ss_n = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds();
-    let DATE_PATTERN_YYYY_MM_DD = "yyyy-mm-dd"
+    const s_globalConfig = null ;
+    const current_datetime = new Date()
+    const DATEFORMATTER_YYYY_MM_DD = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() ;
+    const FORMATTER_YYYY_MM_DD_HH_mm_ss_n = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds();
+    const DATE_PATTERN_YYYY_MM_DD = "yyyy-mm-dd"
   } 
 }
