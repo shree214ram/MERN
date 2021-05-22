573014ac515b62c8af308f0ac9a3f19c6dd968ba
sunny parmar <shree214ram@gmail.com>
2021-05-22 18:27:29 +0530

    Update sapna.js

--- sapna.js
+++ sapna.js
@@ -6,4 +6,24 @@ export default class sapna {
     const FORMATTER_YYYY_MM_DD_HH_mm_ss_n = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds();
     const DATE_PATTERN_YYYY_MM_DD = "yyyy-mm-dd"
   } 
+  
+  // return current time according to Zone Id 
+        function currentDateTimeFn(timeZone) {
+            let currentDateTime = new Date().getTime()
+            const event = new Date(currentDateTime);
+            const options = {
+                day: 'numeric',
+                month: 'long',
+                weekday: 'short',
+                hour: 'numeric',
+                minute: 'numeric',
+                timeZoneName: 'short',
+                timeZone: timeZone,
+            };
+            return new Intl.DateTimeFormat('en-US', options).format(event)
+        }
+
+        return currentDateTimeFn(constants.getTimeZoneNam)
+
+        {getTimeZoneNam: "America/New_York"}
 }
