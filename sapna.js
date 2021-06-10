021cc41000b3ee6840b037907058300c35a3700d
sunny parmar <shree214ram@gmail.com>
2021-06-10 15:41:29 +0530

    Create sapna.js

---
+++ sapna.js
@@ -0,0 +1,28 @@
+
+
+Page A Code 
+url : A.php?a=1&b=2&backToAfromB=true
+componanatDidMount(){
+  const sessionSearchParam = sessionStorage.getItem("searchParam");
+  if(window.params.backToAfromB && sessionSearchParam && type of sessionSearchParam === "object" && Object.keys(sessionSearchParam).length > 0 ){
+    this.searchAdvanceFilter(sessionSearchParam)
+  }
+}
+
+searchAdvanceFilter = (params) => {
+  this.setState({searchParam})
+  sessionStorage.setItem("searchParam", this.state.searchParam);
+  const newUrl = A.php?a=1&b=2
+  window.location.pushState(newUrl) // ye kya karega ki backToAfromB parameter hata. dega url me se 
+}
+
+Page B Code 
+url : B.php?a=1&b=2&backToAfromB=true
+
+
+backToAPage = (params) => {
+  this.setState({searchParam})
+  sessionStorage.setItem("searchParam", this.state.searchParam);
+  const newUrl = A.php?a=1&b=2
+  window.location.pushState(newUrl) // ye kya karega ki backToAfromB parameter hata. dega url me se 
+}
