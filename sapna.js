6fedbdcf3ee289a55b4fd98d00e1f8b3b5ea0911
sunny parmar <shree214ram@gmail.com>
2021-06-11 23:23:25 +0530

    Update sapna.js

--- sapna.js
+++ sapna.js
@@ -4,6 +4,7 @@ Page A Code
 url : A.php?a=1&b=2&backToAfromB=true
 componanatDidMount(){
   const sessionSearchParam = sessionStorage.getItem("searchParam");
+  setState({searchParam})
   if(window.params.backToAfromB && sessionSearchParam && type of sessionSearchParam === "object" && Object.keys(sessionSearchParam).length > 0 ){
     this.searchAdvanceFilter(sessionSearchParam)
   }
@@ -14,6 +15,13 @@ searchAdvanceFilter = (params) => {
   sessionStorage.setItem("searchParam", this.state.searchParam);
   const newUrl = A.php?a=1&b=2
   window.location.pushState(newUrl) // ye kya karega ki backToAfromB parameter hata. dega url me se 
+}
+
+render(){
+  const sessionStorage
+  return(
+  <div><input type="text" value></div>
+  )
 }
 
 Page B Code 
